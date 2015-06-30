9feda841ee9c2b14a450b936eede9bcf23ebf629
Alec Arme & Adam McFadden <student@epicodus.com>
2015-06-30 12:03:14 -0700

    Add default ordering of lessons based on order number

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,4 +3,5 @@ class Lesson < ActiveRecord::Base
   validates :body, presence: true
   validates :order_number, presence: true
   validates :order_number, uniqueness: true
+  default_scope { order('order_number') }
 end
