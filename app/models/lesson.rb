089a3d087d82971f13dc465b9fb999448628c13d
Alec Arme & Adam McFadden <student@epicodus.com>
2015-06-30 14:56:01 -0700

    add next button to lessons.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -6,4 +6,21 @@ class Lesson < ActiveRecord::Base
   validates :order_number, presence: true
   # validates :order_number, uniqueness: true  #no longer using once sections are added.
   default_scope { order('order_number') }
+
+  def next
+    # find all lessons where section id is the same
+    lessons_array = []
+    Lesson.where(section_id: self.section_id).find_each do |lesson|
+      lessons_array.push(lesson)
+    end
+    #   of those, find lesson with order number + 1 and return
+    lessons_array.each do |lesson|
+      if lesson.order_number == self.order_number + 1
+        return lesson
+      end
+    end
+    return self
+  end
+
+
 end
