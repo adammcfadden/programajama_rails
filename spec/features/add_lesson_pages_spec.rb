4f3b3d5e97cb9c1c1bb0d98e945b5c98c2518eb3
Alec Arme & Adam McFadden <student@epicodus.com>
2015-06-30 10:49:43 -0700

    passing integration spec for adding a new lesson

--- spec/features/add_lesson_pages_spec.rb
+++ spec/features/add_lesson_pages_spec.rb
@@ -5,6 +5,7 @@ describe "the add a lesson process" do
     visit lessons_path
     click_on 'Add Lesson'
     fill_in 'Name', :with => '1337 5P34K'
+    fill_in 'Body', :with => 'LESSONS ARE AW350M3'
     click_on 'Create Lesson'
     expect(page).to have_content 'Lessons'
     expect(page).to have_content '1337 5P34K'
