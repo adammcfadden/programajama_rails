cc8bf6e3de0cca41c06dd2eed52b44943d7bce7c
Alec Arme & Adam McFadden <student@epicodus.com>
2015-06-30 11:59:58 -0700

    Add support for editing lessons:

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -27,6 +27,17 @@ class LessonsController < ApplicationController
     redirect_to lessons_path
   end
 
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(lesson_params)
+      redirect_to lesson_path(@lesson)
+    end
+  end
+
   private def lesson_params
     params.require(:lesson).permit(:name, :body, :order_number)
   end
--- spec/features/edit_and_delete_lessons_pages_spec.rb
+++ spec/features/edit_and_delete_lessons_pages_spec.rb
@@ -11,5 +11,21 @@ describe "delete a lesson process" do
     click_on 'Delete'
     expect(page).to_not have_content('1337 5P34K')
   end
+end
 
+describe "edit a lesson" do
+  it "edits a lesson" do
+    visit lessons_path
+    click_on 'Add Lesson'
+    fill_in 'Name', :with => '1337 5P34K'
+    fill_in 'Body', :with => 'LESSONS ARE AW350M3'
+    fill_in 'Order number', :with => '5'
+    click_on 'Create Lesson'
+    click_on 'Edit'
+    fill_in 'Name', :with => 'testname'
+    fill_in 'Body', :with => 'lalalalalalala'
+    fill_in 'Order number', :with => '3'
+    click_on 'Update Lesson'
+    expect(page).to have_content('testname')
+  end
 end
